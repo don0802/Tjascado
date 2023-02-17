fa4fd1f5d8e7709c347244e46f9bde9fb45ba330
Don <don.mathurot@hotmail.com>
2023-02-17 11:24:25 +0100

    Backup

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# Tjascado
+# Ala 3de periode SWD - 4 
+Groep 6
+
+Nico,
+Silas,
+Don,
+Tjeerd
+
+Talen:
+CSS, SQL, PHP
+(Geen bootstrap etc.)
+
+KLEURCODES: 
+
+30/60/10 
+Regel
+
+# 1fb8e9 (Blauwe) 60%
+# 3b3b3b (Grijs) 10%
+# FFFFFF (Wit) 30%
---
+++ pages/main.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <style>
+        @import url("https://use.typekit.net/ewy0arw.css");
+    </style>
+    <meta name="title" content="Tjascado">
+    <meta name="description" content="Tjascado is een website voor ons ALA project die zich plaatsvindt in de derde periode,  wij promoten een energie drankje!">
+    <meta name="keywords" content="EnergieDrank, ALA, Tjascado">
+    <meta name="robots" content="noindex, nofollow">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="language" content="English">
+    <meta name="author" content="Don, Silas, Nico, Tjeerd">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/main.css">
+    <script src="../js/index.js" defer></script>
+    <title>Tjascado Energie</title>
+</head>
+<header id="header">
+    <section class="nav-container">
+        <nav id="navbar">
+            <a href="#" id="logo" title="Tjascado">Tjascado</a>
+            <a href="#" title="Review">Review</a>
+            <a href="#" title="About-us">About-us</a>
+            <a href="#" title="Contact">Contact</a>
+        </nav>
+        <div></div>
+    </section>
+</header>
+<body>
+        <div><video src="" autoplay></div>
+        <div class="blikjes">
+            <img src="">
+            <img src="">
+            <img src="">
+        <div class="informatie-overblikje">
+            <h4>Tjascado energie</h4>
+            <p>Dit lage calorie arme energiedrankje
+                 is een geweldige manier om energie te krijgen zonder te veel 
+                 calorieën te consumeren. Het is gemaakt met natuurlijke 
+                 ingrediënten zoals groene thee extract en guarana om je een
+                 gezonde en natuurlijke energieboost te geven.
+                 Het bevat slechts 15 calorieën per portie, zodat je de energie kunt
+                 genieten zonder je zorgen te maken over het consumeren van te veel suiker of calorieën.
+                 Deze energiedrank is ook verpakt met antioxidanten om je lichaam te beschermen
+                 tegen de schadelijke effecten van vrije radicalen.</p>
+        </div>
+    </div>
+</body>
+<footer>
+    <div class="footer-main">
+        <div class="social-media">
+            <h1>Socials:</h1>
+            <p><a href="https://www.instagram.com/">Instagram</a></p>
+            <p><a href="https://www.twitter.com/">Twitter</a></p>
+            <p><a href="https://www.youtube.com/">YouTube</a></p>
+        </div>
+        <div class="contact">
+            <h1>Contact:</h1>
+                <button id="button-contact"><a href="#">Klik hier om contact op te nemen.</a></button>
+        </div>
+        <div class="About-us">
+            <h1>About us:</h1>
+                <button id="button-contact"><a href="#">Klik hier om meer over ons te weten.</a></button>
+        </div>
+    </div>
+</footer>
+</html>
