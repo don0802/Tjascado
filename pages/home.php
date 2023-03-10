1123abae8f9e6c2bf3fb474fa0202325cb438bbf
Wifi <vissertjeerd123@gmail.com>
2023-03-10 11:29:16 +0100

    d

--- pages/home.php
+++ pages/home.php
@@ -24,13 +24,13 @@
             <source src="../media/video/promotievideo-temp-1.mp4" type="video/mp4">
             </video>
         <nav>
-            <a href="../pages/review.html">Reviews</a>
-            <a href="pages/aboutus.html">About us</a>
-            <a href="pages/contact.html">Contact</a>
+            <a href="../pages/review.php">Reviews</a>
+            <a href="pages/aboutus.php">About us</a>
+            <a href="pages/contact.php">Contact</a>
         </nav>
             <article id="header-art-2">
                 <a><img src="../media/afbeeldingen//Home/search-transparent-white1.png" id="header-img1"></a>
-                <a href="../pages/login.html"><img src="../media/afbeeldingen/Home/profile-transparent-white2.png" id="header-img2"></a>
+                <a href="../pages/login.php"><img src="../media/afbeeldingen/Home/profile-transparent-white2.png" id="header-img2"></a>
             </article>
     </header>
 
