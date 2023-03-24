e85b927daccc2e06a411fb25f0d51b1fa33ad9d0
Nico9009 <113908095+Bigounce909@users.noreply.github.com>
2023-03-24 08:33:35 +0100

    Herstel
    24-03-2023

---
+++ Tjascado restructure/aboutus.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <style>
+        @import url("https://use.typekit.net/ewy0arw.css");
+    </style>
+    <meta name="title" content="Tjascado">
+    <meta name="description" content="Tjascado is een website voor ons ALA project die zich plaatsvindt in de derde periode,  wij promoten een energie drankje!">
+    <meta name="keywords" content="EnergieDrank, ALA, Tjascado">
+    <meta name="robots" content="noindex, nofollow">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="language" content="English">
+    <meta name="author" content="Don, Silas, Nico, Tjeerd">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/main.css">
+    <script src="../js/index.js" defer></script>
+    <title>Tjascado Energie</title>
+</head>
+<body>
+
+</body>
+</html>
